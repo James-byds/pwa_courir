e509b57eaccfcbbb1e8d919ed047e5bf82e78b59
NEPotato <james.boydens@gmail.com>
2025-09-02 13:48:04 +0200

    install_manifest

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import {VitePWA} from "vite-plugin-pwa";
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,45 +12,39 @@ export default defineConfig({
     vueDevTools(),
     //VITE PWA PLUGIN
     VitePWA({
-      registerType: "autoUpdate",
+      registerType: 'autoUpdate',
       manifest: {
-        name: "Je cours pour ma forme",
-        short_name: "Jcpmf",
+        name: 'Je cours pour ma forme',
+        short_name: 'Jcpmf',
         description: "Application pour le suivi d'entrainement de course à pied",
-        theme_color: "#002e4a",
-        background_color: "#88ba16",
-        display: "standalone",
-        scope: "/",
-        start_url: "/",
-        icons: [//48x48 96x96 192x192 550x578
+        theme_color: '#002e4a',
+        background_color: '#88ba16',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          //48x48 96x96 192x192
           {
-            src: "logo-48-48.png",
-            sizes: "48x48",
-            type: "image/png"
+            src: 'icons/logo-48-48.png',
+            sizes: '48x48',
+            type: 'image/png',
           },
           {
-            src: "logo-96-96.png",
-            sizes: "96x96",
-            type: "image/png"
+            src: 'icons/logo-96-96.png',
+            sizes: '96x96',
+            type: 'image/png',
           },
           {
-            src: "logo-192-192.png",
-            sizes: "192x192",
-            type: "image/png"
+            src: 'icons/logo-192-192.png',
+            sizes: '192x192',
+            type: 'image/png',
           },
-          {
-            src: "logo-550-578.png",
-            sizes: "550x578",
-            type: "image/png",
-            purpose: "any maskable"
-          }
         ],
-      }
-    })
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
